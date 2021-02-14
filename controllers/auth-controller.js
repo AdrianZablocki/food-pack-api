393bfca8f8376af1c580d2ee29a73607f604768d
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2021-02-15 00:48:03 +0100

    Remove async functions

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,59 +1,59 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-
-const User = mongoose.model('user');
-
-exports.authUser = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-
-  if (user) {
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-
-    if (validPassword) {
-      const accessToken = jwt.sign(
-        { id: user.id, email: user.email }, process.env.TOKEN_SECRET, { expiresIn: 86400 }
-      );
-      //TODO przy tworzeniu zapisać w bazie danych i przy akcji refresh token sprawdzić czy podany istneje
-      const refreshToken = jwt.sign(
-        { id: user.id, email: user.email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: 525600 }
-      );
-
-      // TODO: CSRF + cookie http only
-      res.cookie('JWT', accessToken, {
-        maxAge: 86400000,
-        httpOnly: true
-      });
-
-      res.json({ accessToken, refreshToken });
-
-    } else {
-      res.json({ message: 'Wrong password' });
-    }
-  } else {
-    res.json({ message: 'Wrong email' });
-  }
-
-
-}
-
-exports.refreshToken = async (req, res) => {
-  const refreshToken = req.body.token;
-  const user = await User.findOne({ _id: req.body.userId });
-
-  if (!refreshToken) {
-    return res.sendStatus(401) // Unauthorized
-  }
-
-  // TODO: check if refresh token exist in DB
-
-  try {
-    await jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-  } catch {
-    return res.sendStatus(403) // bad token forbidden
-  }
-
-  const accessToken = jwt.sign({ id: user.id, email: user.email }, process.env.TOKEN_SECRET, { expiresIn: 86400 })
-
-  res.send({ accessToken })
-}
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const mongoose = require('mongoose');
+//
+// const User = mongoose.model('user');
+//
+// exports.authUser = async (req, res) => {
+//   const user = await User.findOne({ email: req.body.email });
+//
+//   if (user) {
+//     const validPassword = await bcrypt.compare(req.body.password, user.password);
+//
+//     if (validPassword) {
+//       const accessToken = jwt.sign(
+//         { id: user.id, email: user.email }, process.env.TOKEN_SECRET, { expiresIn: 86400 }
+//       );
+//       //TODO przy tworzeniu zapisać w bazie danych i przy akcji refresh token sprawdzić czy podany istneje
+//       const refreshToken = jwt.sign(
+//         { id: user.id, email: user.email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: 525600 }
+//       );
+//
+//       // TODO: CSRF + cookie http only
+//       res.cookie('JWT', accessToken, {
+//         maxAge: 86400000,
+//         httpOnly: true
+//       });
+//
+//       res.json({ accessToken, refreshToken });
+//
+//     } else {
+//       res.json({ message: 'Wrong password' });
+//     }
+//   } else {
+//     res.json({ message: 'Wrong email' });
+//   }
+//
+//
+// }
+//
+// exports.refreshToken = async (req, res) => {
+//   const refreshToken = req.body.token;
+//   const user = await User.findOne({ _id: req.body.userId });
+//
+//   if (!refreshToken) {
+//     return res.sendStatus(401) // Unauthorized
+//   }
+//
+//   // TODO: check if refresh token exist in DB
+//
+//   try {
+//     await jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+//   } catch {
+//     return res.sendStatus(403) // bad token forbidden
+//   }
+//
+//   const accessToken = jwt.sign({ id: user.id, email: user.email }, process.env.TOKEN_SECRET, { expiresIn: 86400 })
+//
+//   res.send({ accessToken })
+// }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,32 +1,45 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 const User = mongoose.model('user');
-const saltRounds = 12
+// const saltRounds = 12
 
-exports.createUser = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-
-  if (!user) {
-    bcrypt.hash(req.body.password, saltRounds, (err,   hash) => {
-      new User({
-        email: req.body.email,
-        password: hash
-      }).save((err, data) => {
-        if (err) {
-          res.status(500).json({
-            message: 'Something went wrong, try again later'
-          });
-        } else {
-          const userData = { ...data._doc }
-          delete userData.password
-          res.status(200).json({ message: 'User created', userData })
-        }
-      })
-    });
-  } else {
-    res.status(409).json({
-      message: `User with email: ${req.body.email} exist`
-    });
-  }
+exports.createUser = (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    console.log(user);
+    if (!user) {
+      res.status(200).json({ message: 'User created' })
+    } else {
+      res.status(409).json({
+        message: `User with email: ${req.body.email} exist`
+      });
+    }
+  });
 }
+
+// exports.createUser = async (req, res) => {
+//   const user = await User.findOne({ email: req.body.email }).then(res => console.log(res));
+//
+//   if (!user) {
+//     bcrypt.hash(req.body.password, saltRounds, (err,   hash) => {
+//       new User({
+//         email: req.body.email,
+//         password: hash
+//       }).save((err, data) => {
+//         if (err) {
+//           res.status(500).json({
+//             message: 'Something went wrong, try again later'
+//           });
+//         } else {
+//           const userData = { ...data._doc }
+//           delete userData.password
+//           res.status(200).json({ message: 'User created', userData })
+//         }
+//       })
+//     });
+//   } else {
+//     res.status(409).json({
+//       message: `User with email: ${req.body.email} exist`
+//     });
+//   }
+// }
