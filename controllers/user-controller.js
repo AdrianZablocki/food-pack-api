68f78dbafcda03bd242798526178b0f8011a316e
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2021-02-15 23:26:27 +0100

    Fix

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,7 +12,7 @@ exports.createUser = function(req, res) {
     .then(client => {
     console.log('Connected to Database')
     const db = client.db('test')
-    db.collection('users').find({ email: req.body.email }).toArray().then(col => res.status(200).json({ message: col }));
+    db.collection('users').find({ email: req.body.email }).toArray().then(col => res.status(200).json({ message: col, kurwa: "I huj ci do tego" }));
   })
   .catch(error => console.error(error))
 }
