ffcfc0dd4a473859bf9028d3a2d7aee981e35ac3
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2021-02-15 22:43:47 +0100

    Fix

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,30 +13,31 @@ exports.createUser = async function(req, res) {
     console.log('Connected to Database')
     const db = client.db('test')
     db.collection('users').find({ email: req.body.email }).toArray().then(user => {
-      if (!user) {
-        res.status(200).json({ message: 'User created', user })
-        // bcrypt.hash(req.body.password, saltRounds, (err,   hash) => {
-        //   db.collection('users').insertOne({
-        //     email: req.body.email,
-        //     password: hash
-        //   }, (err, result) => {
-        //     if (err) {
-        //       res.status(500).json({
-        //         message: 'Something went wrong, try again later'
-        //       });
-        //     } else {
-        //       // const userData = { ...data._doc }
-        //       // delete userData.password
-        //       res.status(200).json({ message: 'User created', result })
-        //     }
-        //   })
-        // });
-      } else {
-        res.status(409).json({
-          message: `User with email: ${user} exist`
-        });
-      }
-      // res.status(200).json({ message: user }));
+
+      // if (!user) {
+      //   res.status(200).json({ message: 'User created', user })
+      //   // bcrypt.hash(req.body.password, saltRounds, (err,   hash) => {
+      //   //   db.collection('users').insertOne({
+      //   //     email: req.body.email,
+      //   //     password: hash
+      //   //   }, (err, result) => {
+      //   //     if (err) {
+      //   //       res.status(500).json({
+      //   //         message: 'Something went wrong, try again later'
+      //   //       });
+      //   //     } else {
+      //   //       // const userData = { ...data._doc }
+      //   //       // delete userData.password
+      //   //       res.status(200).json({ message: 'User created', result })
+      //   //     }
+      //   //   })
+      //   // });
+      // } else {
+      //   res.status(409).json({
+      //     message: `User with email: ${user} exist`
+      //   });
+      // }
+      res.status(200).json({ message: user }));
   })
   .catch(error => console.error(error))
 }
