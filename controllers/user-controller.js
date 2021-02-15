3b26d8d1ca38a7b2a712e3cfdadde911cf67c6a1
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2021-02-15 10:32:30 +0100

    Try

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,11 +4,30 @@ const mongoose = require('mongoose');
 const User = mongoose.model('user');
 // const saltRounds = 12
 
-exports.createUser = function(req, res) {
-  const user = User.findOne({ email: req.body.email }).then(function(user) {
-    console.log(user);
-    res.json({ message: 'Huj w to wbijam', user });
-  });
+exports.createUser = async function(req, res) {
+  // const user = await User.findOne({ email: req.body.email })
+  //   if (!user) {
+  //     res.status(200).json({ message: 'User created' })
+  //   } else {
+  //     res.status(409).json({
+  //       message: `User with email: ${req.body.email} exist`
+  //     });
+  //   }
+
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+      res.status(200).json({ message: 'User created' })
+    } else {
+      res.status(409).json({
+        message: `User with email: ${req.body.email} exist`
+      });
+    }
+
+  } catch {
+    return res.sendStatus(404) // bad request
+  }
+
   // res.json({ message: 'Huj w to wbijam' });
   // User.findOne({ email: req.body.email }).then(function(user) {
   //   if (!user) {
