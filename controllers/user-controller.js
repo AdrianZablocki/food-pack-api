d4f02f6df099945a9f1dec13b4b95e4120951fdd
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2021-02-15 16:46:21 +0100

    Try

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,8 +5,7 @@ const mongoose = require('mongoose');
 const User = mongoose.model('user');
 // const saltRounds = 12
 
-exports.createUser = async function(req, res) {
-  const user = await User.findOne({ email: req.body.email })
+exports.createUser = function(req, res) {
   res.status(200).json({ message: 'Crete user' })
  // User.findOne({ email: req.body.email })
  //  .then(user => {
