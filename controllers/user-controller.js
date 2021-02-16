04297a0278148723eef6cfd49a686b7e1b45e418
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2021-02-16 22:07:52 +0100

    Fix

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,4 @@
-// const asyncHandler = require('express-async-handler')
-// const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-
-// const User = mongoose.model('users');
-// const saltRounds = 12
-
 const MongoClient = require('mongodb').MongoClient
 
 exports.createUser = function(req, res) {
@@ -13,32 +7,16 @@ exports.createUser = function(req, res) {
       console.log('Connected to Database')
       const db = client.db('test')
       db.collection('users').findOne({ email: req.body.email }).then(col => {
-        // res.status(200).json({ message: col, kurwa: "I huj ci do tego" });
         if (!col) {
           bcrypt.genSalt(10, function(err, salt) {
             bcrypt.hash(req.body.password, salt, function(err, hash) {
-                // Store hash in your password DB.
-                res.status(200).json({ message: 'User created', hash })
+                db.collection('users').insertOne({
+                  email: req.body.email,
+                  password: hash
+                }).then(() => res.status(200).json({ message: 'User created' }))
+                  .catch(() => res.status(500).json({ message: 'Something went wrong, try again later' }))
             });
           });
-          // res.status(200).json({ message: 'User created'})
-          // bcrypt.hash(req.body.password, saltRounds, (err,   hash) => {
-          //   res.status(200).json({ message: 'User created', hash })
-          //   // new User({
-          //   //   email: req.body.email,
-          //   //   password: hash
-          //   // }).save((err, data) => {
-          //   //   if (err) {
-          //   //     res.status(500).json({
-          //   //       message: 'Something went wrong, try again later'
-          //   //     });
-          //   //   } else {
-          //   //     const userData = { ...data._doc }
-          //   //     delete userData.password
-          //   //     res.status(200).json({ message: 'User created', userData })
-          //   //   }
-          //   // })
-          // });
         } else {
           res.status(409).json({
             message: `User with email: ${req.body.email} exist`
