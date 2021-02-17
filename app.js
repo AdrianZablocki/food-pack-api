fc5b495539f673f1e6a825612cd6d9f3618514d8
ADRIAN ZABLOCKI <adrian.zablocki@execon.pl>
2021-02-17 17:11:20 +0100

    Fix

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
@@ -18,18 +17,13 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// app setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
